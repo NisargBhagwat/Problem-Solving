758fc50df871ee8451496271094ead7cadfed0a3
Nisarg <bhagwatnisarg99@gmail.com>
2022-10-15 22:57:54 +0530

    subset sum problem solved

---
+++ Rec7SubSetSum1.js
@@ -0,0 +1,24 @@
+function subSequece(index, arr, candidates, sum, ans) {
+    if (index >= candidates.length) {
+        ans.push(sum);
+        return;
+    }
+
+    const indexValue = candidates[index];
+    const nextIndex = index + 1;
+
+    arr.push(indexValue);
+    subSequece(nextIndex, arr, candidates, sum + indexValue, ans);
+
+    arr.pop();
+    subSequece(nextIndex, arr, candidates, sum, ans);
+}
+
+const candidates = [1, 2, 3];
+let tempArr = [];
+let ans = [];
+let sum = 0;
+
+subSequece(0, tempArr, candidates, sum, ans);
+
+console.log(ans.sort((a, b) => a - b));
